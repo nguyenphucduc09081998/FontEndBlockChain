ea274434d3ca92069d0b6ceaa759dc43e3c2048f
DESKTOP-OR0SSCH\PhucDuc <phucducktpm@gmail.com>
2020-05-19 22:26:23 +0700

    40% sreen login

--- App.js
+++ App.js
@@ -5,110 +5,161 @@
  * @format
  * @flow strict-local
  */
-
-import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Header, LearnMoreLinks, Colors, DebugInstructions,ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+import React, { Component } from 'react';
+import {
+  SafeAreaView, Image, StyleSheet, ScrollView, View,Text, ImageBackground, StatusBar, 
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+const {width: WIDTH} = Dimensions.get('window');
+import imageground from './images/background.jpg';
+import logologin from './images/blockchain.jpg';
+const logo = { uri: "https://reactjs.org/logo-og.png" };
+
+export default class Login extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      showPass: true,
+      press: false
+    }
+  }
+
+  showPass = () => {
+    if(this.state.press == false){
+      this.setState({showPass: false, press: true})
+    }else{
+      this.setState({showPass: true, press: false})
+    }
+  }
+
+  render () {
+    return (
+      <ImageBackground source={imageground} style={styles.imagegroundContainer}>
+    
+        <View style={styles.logoContainer}>
+          <Image source={logologin} style={styles.logo} />
+          <Text style={styles.titlelogin}>Block Chain</Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Icon name={'ios-person-outline'} size={28}
+                color={'rgba(255,255,255,0.7)'}
+                style={styles.inputIcon}
+            />  
+          <TextInput
+            style={styles.input}
+            placeholder={'Username'}
+            placeholderTextColor={'rgba(255,255,255,0.7)'}
+            underlineColorAndroid='transparent'
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Icon name={'person'} size={28}
+                color={'rgba(255,255,255,0.7)'}
+                style={styles.inputIcon}
+            />  
+          <TextInput
+            style={styles.input}
+            placeholder={'Password'}
+            secureTextEntry={this.state.showPass}
+            placeholderTextColor={'rgba(255,255,255,0.7)'}
+            underlineColorAndroid='transparent'
+          />
+
+          <TouchableOpacity style={styles.btneye} onPress={this.showPass.bind(this)}>
+            <Icon name={ this.state.press == false ? 'ios-eye-outline' : 'ios-eye-off-outline'}  
+                size={26} color={'rgba(255,255,255,0.7)'}
+            />
+          </TouchableOpacity >
+        </View>
+
+        <TouchableOpacity style={styles.btnLogin}>
+          <Text style={styles.text}>Login</Text>
+        </TouchableOpacity>
+
+      </ImageBackground>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  imagegroundContainer: {
+    flex:1,
+    width:null,
+    height:null,
+    justifyContent: 'center',
+    alignItems:'center',
   },
-  engine: {
+
+  logo: {
+    width:120,
+    height:120,
+  },
+
+  logoContainer: {
+    alignItems:'center',
+    marginBottom:50
+  },
+  
+  titlelogin: {
+    color: 'white',
+    fontSize:20,
+    fontFamily:'500',
+    marginTop: 10,
+    opacity:0.5,
+  },
+
+  input: {
+    width: WIDTH - 55,
+    height:45,
+    borderRadius:25,
+    fontSize:16,
+    paddingLeft:45,
+    backgroundColor: 'rgba(0,0,0,0.35)',
+    color: 'rgba(255,255,255,0.7)',
+    marginHorizontal:25,
+  },
+
+  inputContainer: {
+    marginTop:10
+  },
+
+  inputIcon: {
     position: 'absolute',
-    right: 0,
+    top: 8,
+    left:37,
   },
-  body: {
-    backgroundColor: Colors.white,
+
+  btneye: {
+    position: 'absolute',
+    top: 8,
+    right:37,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+
+  btnLogin: {
+    width: WIDTH - 55,
+    height:45,
+    borderRadius:25,
+    backgroundColor: '#432577',
+    justifyContent: 'center',
+    marginTop: 20
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+
+  text: {
+    color:  'rgba(255,255,255,0.7)',
+    fontSize:16,
+    textAlign:'center'
+  }
+  
+ 
 });
 
-export default App;
---
+++ Instruction.md
@@ -0,0 +1,10 @@
+## --- reset react native  --- 
+# npm start -- --reset-cache
+# react-native start --reset-cache
+# react-native link
+
+
+## --- Add Icon ---
+#   yarn add react-native-vector-icons
+#   npm install react-native-vector-icons --save
+## 
