b08188e90970067abaf6de70359b310bcdb7f1f2
DESKTOP-OR0SSCH\PhucDuc <phucducktpm@gmail.com>
2020-06-07 20:17:19 +0700

    code nativasion

---
+++ src/components/Register.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import {StyleSheet,Text,View,TouchableOpacity,TextInput} from 'react-native'
+
+export default class Register extends Component {
+    render(){
+        return(
+            <View style={styles.Register}>
+                <Text style={styles.header}>Register</Text>
+                <TextInput style={styles.textinput} placeholder="Private Key" 
+                    underlineColorAndroid={'transparent'} />
+
+                <TextInput style={styles.textinput} placeholder="Public Key" 
+                    underlineColorAndroid={'transparent'} />    
+
+                <TextInput style={styles.textinput} placeholder="Message" 
+                   secureTextEntry={true} underlineColorAndroid={'transparent'} />    
+                
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.btntext}>Sign Up</Text>
+                </TouchableOpacity>
+
+            </View>    
+        );
+    } 
+}
+
+const styles = StyleSheet.create({
+    Register :{
+        alignSelf:'stretch',
+
+    },  
+    header:{
+        fontSize:24,
+        color:'#fff',
+        paddingBottom:10,
+        marginBottom:40,
+        borderBottomColor:'#199187',
+        borderBottomWidth:1,
+    },
+    textinput:{
+        alignSelf:'stretch',
+        height:40,
+        marginBottom:30,
+        color:'#fff',
+        borderBottomColor: '#f8f8f8',
+        borderBottomWidth: 1,
+    },
+    button:{
+        alignSelf:'stretch',
+        alignItems:'center',
+        padding:20,
+        backgroundColor:'#59cbbd',
+        marginTop: 30,
+    },
+    btntext:{
+        color:'#fff',
+        fontWeight:'bold',
+    }
+
+})
